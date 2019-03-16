bb636e39fca082dcd1725664c4ad238a488654ee
Shawn Xu <z5142629@unsw.edu.au>
2019-03-16 18:49:40 +1100

    add backend draft

---
+++ backend/app/demo/v1/routes.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+###
+### DO NOT CHANGE THIS FILE
+### 
+### The code is auto generated, your change will be overwritten by 
+### code generating.
+###
+from __future__ import absolute_import
+
+from .api.knowledge_base import KnowledgeBase
+from .api.knowledge_base_KID import KnowledgeBaseKid
+from .api.knowledge_base_KID_document import KnowledgeBaseKidDocument
+from .api.knowledge_base_KID_document_DID import KnowledgeBaseKidDocumentDid
+
+
+routes = [
+    dict(resource=KnowledgeBase, urls=['/knowledge_base'], endpoint='knowledge_base'),
+    dict(resource=KnowledgeBaseKid, urls=['/knowledge_base/<KID>'], endpoint='knowledge_base_KID'),
+    dict(resource=KnowledgeBaseKidDocument, urls=['/knowledge_base/<KID>/document'], endpoint='knowledge_base_KID_document'),
+    dict(resource=KnowledgeBaseKidDocumentDid, urls=['/knowledge_base/<KID>/document/<DID>'], endpoint='knowledge_base_KID_document_DID'),
+]
