156a640f68800e752f41bc14f52bff82597cc919
Tim Chen <tianpeng.chen1995@gmail.com>
2019-04-16 22:36:45 +1000

    remove unuseful files to minimise cloud disk quota

--- backend/stackexchange-api/export_from_mongodb.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-mongoexport --uri='mongodb://CloudFoundry_m95ffoob_m60kfgms_jjmk0td8:3jBJb4WNSEmVCTvEd3LFLkgsq-uVDOIS@ds133556.mlab.com:33556/CloudFoundry_m95ffoob_m60kfgms' --collection stackoverflow_records --out python.json
